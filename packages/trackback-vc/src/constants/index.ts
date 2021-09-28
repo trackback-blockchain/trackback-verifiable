ac4ed6e6f00f515f009bcab56e9d0e48af6006e9
Don <don@track-back.co>
2021-09-28 16:29:29 +1300

    basic credential check

--- packages/trackback-vc/src/constants/index.ts
+++ packages/trackback-vc/src/constants/index.ts
@@ -1,2 +1,6 @@
 export const CONTENT_TYPE_DID_LD_JSON = 'application/did+ld+json'
 export const CONTENT_TYPE_DID_JSON = 'application/did+json'
+
+export const CREDIENTIAL_CONTEXT = "https://www.w3.org/2018/credentials/v1"
+
+export const RFC3339_REGEX = /^((?:(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}(?:\.\d+)?))(Z|[\+-]\d{2}:\d{2})?)$/gm
---
+++ packages/trackback-vc/src/vc/check.ts
@@ -0,0 +1,56 @@
+
+
+import { RFC3339_REGEX, CREDIENTIAL_CONTEXT } from '../constants';
+
+function isNonEmptyArray(item: any): boolean {
+    return Array.isArray(item) && item.length > 0
+}
+
+
+export function check(credential: any): boolean {
+    if (!credential) {
+        throw new Error('credential required')
+    }
+
+    if (!credential["@context"] || !isNonEmptyArray(credential["@context"])) {
+        throw new Error('Credentials must have a "@context" property.');
+    }
+
+    const context = credential['@contextt'][0];
+
+    if (context !== CREDIENTIAL_CONTEXT) {
+        throw new Error("Credentials first item is a URI with the value https://www.w3.org/2018/credentials/v1");
+    }
+
+    if (!credential["type"] || !isNonEmptyArray(credential["type"])) {
+        throw new Error('Credentials must have "type".');
+    }
+
+
+    if (!credential['type'].includes("VerifiableCredential")) {
+        throw new Error('"type" must have VerifiableCredential');
+    }
+
+    if (!credential["credentialSubject"]) {
+        throw new Error('Credentials must have "credentialSubject"');
+    }
+
+    if (!credential["issuer"]) {
+        throw new Error('Credentials must have "issuer"');
+    }
+
+    if (credential["issuanceDate"]) {
+        if (typeof credential["issuanceDate"] !== 'string') {
+            throw new Error('"issuanceDate" must be a string.');
+        }
+
+        if (!RFC3339_REGEX.test(credential["issuanceDate"])) {
+            throw new Error('"issuanceDate" must be RFC 3339 Date Time.');
+        }
+    }
+
+    return true;
+
+}
+
+
