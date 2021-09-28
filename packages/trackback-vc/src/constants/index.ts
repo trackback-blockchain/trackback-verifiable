6328ea08ff7d50a5992fe7e26264db1d3426ea06
Don <don@track-back.co>
2021-09-28 16:31:28 +1300

    rfc3339 regex

--- packages/trackback-vc/src/constants/index.ts
+++ packages/trackback-vc/src/constants/index.ts
@@ -3,4 +3,5 @@ export const CONTENT_TYPE_DID_JSON = 'application/did+json'
 
 export const CREDIENTIAL_CONTEXT = "https://www.w3.org/2018/credentials/v1"
 
+//https://regex101.com/r/qH0sU7/1
 export const RFC3339_REGEX = /^((?:(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}(?:\.\d+)?))(Z|[\+-]\d{2}:\d{2})?)$/gm
